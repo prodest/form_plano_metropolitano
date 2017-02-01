3067bffe3ededafef1911b952e02bd54756cc27e
vizeke <vizeke@gmail.com>
2017-02-01 10:21:16 -0200

    Adiciona verificação de usuário ao form

--- src/app/form-sugestao/form-sugestao.component.ts
+++ src/app/form-sugestao/form-sugestao.component.ts
@@ -1,6 +1,9 @@
 import 'rxjs/Rx';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'oidc-client';
 import { MapeandoESService } from '../shared/mapeandoes.service';
+import { AuthenticationService } from '../shared/authentication.service';
 import { Demand } from './model/demand';
 import { IDistrict, ITheme } from '@prodest/mapeandoes-typings';
 import { settings } from '../shared/settings';
@@ -8,64 +11,8 @@ import { settings } from '../shared/settings';
 @Component( {
     moduleId: module.id,
     selector: 'form-sugestao',
-    providers: [ MapeandoESService ],
-    template: `<div class="col-lg-10 col-lg-offset-1">
-    <form>
-       
-        <div class="form-group">
-            <label for="category">Categoria</label>
-            <select name="category" class="form-control" required [(ngModel)]="model.categoryId">
-                <option *ngFor="let category of categories" [value]="category.id">{{category.name}}</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <div class="row">
-            <div class="col-sm-6">
-                <label for="themes">Tema(s)</label>
-                <br>
-                <div class="btn-group btn-group-vertical" data-toggle="buttons">
-                    <label class="btn" *ngFor="let theme of themes">
-                        <input type="checkbox" name='{{theme.name}}'value="{{theme.id}}"  (click)="toggleThemes($event, theme)"/><i class="fa fa-square-o fa-2x"></i><i class="fa fa-check-square-o fa-2x"></i> <span> {{theme.name}}</span>
-                    </label>                     
-
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <label for="districts">Município(s)</label>
-                <br>
-                <div class="btn-group btn-group-vertical" data-toggle="buttons">
-                    <label class="btn" *ngFor="let district of districts">
-                        <input type="checkbox" name='{{district.name}}'value="{{district.id}}" (click)="toggleDistrict($event, disctrict)"/><i class="fa fa-square-o fa-2x"></i><i class="fa fa-check-square-o fa-2x"></i> <span> {{district.name}}</span>
-                    </label>                     
-
-                </div>
-            </div>
-            </div>
-
-        </div>
-
-        <div class="form-group">
-            <label for="source">Órgão</label>
-            <select name="source" class="form-control" required [(ngModel)]="model.sourceId">
-                <option *ngFor="let source of sources" [value]="source.id">{{source.name}}</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="title">Título</label>
-            <input type="text" class="form-control" id="title" [(ngModel)]="model.title" required name="name">
-        </div>
-        <div class="form-group">
-            <label for="description">Descrição</label>
-            <textarea type="text" class="form-control" id="description" [(ngModel)]="model.description" name="text"></textarea>
-        </div>
-        <div class="form-group text-right">
-            <button type="submit" class="btn btn-primary btn-md background-primary-button-background-color primary-button-border-border-color font-color-primary-button-color btn-submit" (click)="sendDemand(model)">
-                Enviar <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-            </button>
-        </div>
-
-    </form>
-</div>`,
+    providers: [ MapeandoESService, AuthenticationService ],
+    templateUrl: 'form-sugestao.template.html',
     styleUrls: [ settings.orchardModulePath + 'form-sugestao.component.css' ]
 })
 export class FormSugestaoComponent implements OnInit {
@@ -85,7 +32,15 @@ export class FormSugestaoComponent implements OnInit {
         pins: []
     };
 
-    constructor( private mapeandoESService: MapeandoESService ) {
+    constructor( private router: Router,
+                 private mapeandoESService: MapeandoESService,
+                 private authenticationService: AuthenticationService ) {
+        this.authenticationService.userLoadededEvent.subscribe(( user: User ) => {
+            if ( !user ) {
+                this.router.navigate( [ 'login' ] );
+            }
+        });
+        this.authenticationService.getUser();
     }
 
     ngOnInit() {
