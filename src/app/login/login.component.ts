389ab7c8b01f8263b208279441baa8bd115197de
vizeke <vizeke@gmail.com>
2017-02-01 10:21:46 -0200

    Remove list-demand do login.component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,21 +3,19 @@ import { Router } from '@angular/router';
 import { AuthenticationService } from '../shared/authentication.service';
 import { User } from 'oidc-client';
 import { settings } from '../shared/settings';
-import { ListSugestaoComponent } from '../list-sugestao/list-sugestao.component';
 
 @Component( {
     moduleId: module.id,
     selector: 'login',
-    providers: [ AuthenticationService, ListSugestaoComponent ],
+    providers: [ AuthenticationService ],
     template: `<div class="text-center">
-                    <p>Para fazer sua sugestão é necessário entrar com sua conta do <b>Acesso Cidadão ES</b></p>
-                    <div class="recent-read-more learn-more">
-                    <button (click)="login()" 
-                            title="Entrar pelo Acesso Cidadão" 
-                            class="btn btn-primary btn-md background-primary-button-background-color primary-button-border-border-color font-color-primary-button-color">Entrar com o Acesso Cidadão</button>
-                    </div>    
-               </div>
-               <list-sugestao></list-sugestao>`,
+                   <p>Para fazer sua sugestão é necessário entrar com sua conta do <b>Acesso Cidadão ES</b></p>
+                   <div class="recent-read-more learn-more">
+                   <button (click)="login()" 
+                           title="Entrar pelo Acesso Cidadão"
+                           class="btn btn-primary btn-md background-primary-button-background-color primary-button-border-border-color font-color-primary-button-color">Entrar com o Acesso Cidadão</button>
+                   </div>    
+               </div>`,
     styleUrls: [ settings.orchardModulePath + 'login.component.css' ]
 })
 export class LoginComponent {
@@ -32,7 +30,6 @@ export class LoginComponent {
         private router: Router,
         private authenticationService: AuthenticationService ) {
 
-        console.log( 'constructor LoginComponent' );
         this.authenticationService.userLoadededEvent.subscribe(( user: User ) => {
             if ( user ) {
                 this.router.navigate( [ 'form-sugestao' ] );
