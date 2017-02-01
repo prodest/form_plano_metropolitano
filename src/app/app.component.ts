542b897f24a1323d5c49dd467787910e161c996f
vizeke <vizeke@gmail.com>
2017-02-01 10:20:57 -0200

    Altera layout externo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,9 @@ import { settings } from './shared/settings';
 @Component({
   moduleId: module.id,
   selector: 'my-app',
-  template: `<router-outlet></router-outlet>`,
+  template: `<router-outlet></router-outlet>
+             <hr />
+             <list-sugestao></list-sugestao>`,
   styleUrls: [ settings.orchardModulePath + 'app.component.css' ]
 })
 export class AppComponent  { name = 'Angular'; }
